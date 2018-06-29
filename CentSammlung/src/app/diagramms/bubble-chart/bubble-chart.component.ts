d2cd0bf2661b4dba796d956dae276a94c562966e
Steffen Biegler <Steffen.Biegler@deutschebahn.com>
2018-06-29 11:00:23 +0200

    bubbeling

--- CentSammlung/src/app/diagramms/bubble-chart/bubble-chart.component.ts
+++ CentSammlung/src/app/diagramms/bubble-chart/bubble-chart.component.ts
@@ -12,7 +12,7 @@ export class BubbleChartComponent implements OnInit {
   @Input() private data: Array < any > ;
   private colors;
   private columnwidht = 20;
-  private margin: any = { top: 20, bottom: 20, left: 40, right: 20};
+  private margin: any = { top: 20, bottom: 20, left: 20, right: 20};
   constructor() {}
 
   ngOnInit() {
@@ -35,17 +35,26 @@ export class BubbleChartComponent implements OnInit {
       .selectAll('circle')
       .data(this.data)
       .enter();
-    circle.append('circle')
-      .attr('r', data => (data[1] / 300))
+    const bubble = circle.append('circle')
+      .attr('r',  1)
       .attr('fill', (d, i) => {
         return this.colors(d[0]);
       })
       .attr('stroke', '#333')
       .attr('transform', (data, index) => {
+        const offsetX =  this.margin.left;
+        const offsetY =  reverseOffset;
+        return `translate(${offsetX}, ${offsetY})`;
+      });
+      bubble.transition()
+      .delay((d, i) => i * 100)
+      .attr('transform', (data, index) => {
         const offsetX = ((data[2] - 1999) * (10 + this.columnwidht)  + data[1] / 300) + this.margin.left;
         const offsetY =  reverseOffset - data[1] / 20 ;
         return `translate(${offsetX}, ${offsetY})`;
-      }).append('title').text((d) => d[0] + ' ' + d[2] + ':' + d[1]);
+      })
+      .attr('r', data => (data[1] / 250));
+      bubble.append('title').text((d) => d[0] + ' ' + d[2] + ':' + d[1]);
 
     /*circle.append('text')
     .text((d) => d[0] + ' ' + d[2])
