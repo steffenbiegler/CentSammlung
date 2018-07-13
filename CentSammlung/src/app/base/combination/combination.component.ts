f33124386513581052db4e68ec049f2cab6dc2e9
BKU\SteffenBiegler <steffen.biegler@gmail.com>
2018-07-13 10:25:07 +0200

    playing with combinations

--- CentSammlung/src/app/base/combination/combination.component.ts
+++ CentSammlung/src/app/base/combination/combination.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {  Component,  OnInit} from '@angular/core';
+import {  LAENDER} from '../../constants';
 
 @Component({
   selector: 'app-combination',
@@ -6,14 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./combination.component.css']
 })
 export class CombinationComponent implements OnInit {
+  startYear = 1999;
+  endYear = (new Date()).getFullYear();
+  year = this.endYear;
+  countries = [];
 
-  constructor() { }
+  constructor() {
+    this.countries = LAENDER;
+
+  }
 
   ngOnInit() {
+
+  }
+  log(m) {
+    console.log(m);
   }
 
 }
-
-
-
-
